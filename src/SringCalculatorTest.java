987812aee36580689ac4a1f18cd13f19b2163ab4
jean-michel Delorme <jm.delorme@ymail.com>
2016-05-24 14:48:09 +0200

    Add second text

--- src/SringCalculatorTest.java
+++ src/SringCalculatorTest.java
@@ -1,19 +1,34 @@
 import static org.junit.Assert.*;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class SringCalculatorTest {
-
+	private StringCalculator stringCalculator;
+	private final String TWO_NUMBERS_TO_ADD="2,1";
+@Before
+public void setup(){
+		stringCalculator = new StringCalculator();
+}
 @Test
-public void AddEmpty(){
+public void addEmpty(){
 	// Given
-	StringCalculator sc = new StringCalculator();
+	
 	// When
-	int res = sc.add("");
+	int res = stringCalculator.add("");
 	// Then
 	assertEquals(0,res);
 }
 
+@Test
+public void addTwoNumbers(){
+	// Given
 	
+	// When
+	int res = stringCalculator.add(TWO_NUMBERS_TO_ADD);
+	// Then
+	assertEquals(3,res);
+}
 	
 
 }
